48586fcdbb96333767074a73bba9af28d57ded74
Lucas Costa <lucas6.silva@alunos.ifsuldeminas.edu.br>
2025-09-23 21:45:13 -0300

    initial node

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware para processar JSON nas requisições
+app.use(express.json());
+
+// Rota de teste
+app.get('/', (req, res) => {
+  res.send('Olá, mundo! Sua API está funcionando!');
+});
+
+// Inicia o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
