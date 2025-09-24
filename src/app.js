f4e582b065100c74974453a58666628f597bd526
Lucas Costa <lucas6.silva@alunos.ifsuldeminas.edu.br>
2025-09-24 19:41:06 -0300

    feat: included new route user routes

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 
 const gamesRoutes = require('./routes/gamesRoutes');
+const usersRoutes = require('./routes/usersRoutes');
 
 app.use(express.json());
 
-app.use('/games', gamesRoutes);
+app.use('/api/games', gamesRoutes);
+app.use('/api/users', usersRoutes);
 
 app.get('/', (req, res) => {
   res.send('Olá, mundo! Sua API está funcionando!');
