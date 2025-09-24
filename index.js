83d9eff9e75eb0b0fd21b85cae0e4d0f60263446
Lucas Costa <lucas6.silva@alunos.ifsuldeminas.edu.br>
2025-09-23 22:30:12 -0300

    refactor: remove routes

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-const app = require('express')();
-const PORT = 3000;
+const app = require('./src/app');
+const PORT = process.env.PORT || 3000;
 
-app.listen(
-  PORT,
-  () => console.log(`it's alive on http://localhost:${PORT}`)
-)
-
-app.get('/', (req, res) => {
-  res.send('Olá, mundo! Sua API está funcionando!');
-});
-
-app.get('/test', (req, res) => {
-  res.status(200).send({
-    label: 'Hello World',
-    status: 'success',
-  });
+app.listen(PORT, () => {
+  console.log(`it's alive on http://localhost:${PORT}`);
 });
