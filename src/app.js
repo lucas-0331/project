b82efba6114b1be24a5e6296ebb15fccbc0eb8b7
Lucas Costa <lucas6.silva@alunos.ifsuldeminas.edu.br>
2025-09-25 20:26:28 -0300

    feat: config swagger

--- src/app.js
+++ src/app.js
@@ -1,16 +1,23 @@
 const express = require('express');
+const YAML = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
+
 const app = express();
+
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 const gamesRoutes = require('./routes/gamesRoutes');
 const usersRoutes = require('./routes/usersRoutes');
 
 app.use(express.json());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use('/api/games', gamesRoutes);
 app.use('/api/users', usersRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Olá, mundo! Sua API está funcionando!');
+  res.send('Olá, mundo! API está funcionando!');
 });
 
 module.exports = app;
