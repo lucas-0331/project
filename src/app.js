3c7480d0eb56a8c48e3529a8357d0ec4454069c2
Lucas Costa <lucas6.silva@alunos.ifsuldeminas.edu.br>
2025-10-13 20:27:23 -0300

    fix: documentation api docs

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const YAML = require('yamljs');
+const fs = require('fs');
+const cors = require('cors');
+const YAML = require('yaml');
 const swaggerUi = require('swagger-ui-express');
 const routes = require('./routes/routes');
-
+const file = fs.readFileSync('./swagger.yaml', 'utf8');
+const swaggerDocument = YAML.parse(file);
 const app = express();
 
-const swaggerDocument = YAML.load('./swagger.yaml');
-
 app.use(express.json());
+app.use(cors());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
     res.status(200).json({
@@ -25,7 +28,5 @@ app.use((req, res, next) => {
         path: req.originalUrl
     });
 });
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
