b48cfac08633553b2b8dd058d30f588de3eb7587
Lucas Costa <lucas6.silva@alunos.ifsuldeminas.edu.br>
2025-10-23 00:19:27 -0300

    feat: created new parameters and new rules to get games in database

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -1,35 +1,56 @@
-const axios = require('axios');
 const {PrismaClient} = require('@/generated/prisma/client');
 const prisma = new PrismaClient();
 
-const getAllGames = async (req, res) => {
-    const { cursor } = req.query;
-    try {
+const getGames = async (req, res) => {
+    const take = 30;
+    const {cursor, sortBy = 'id', setOrder = 'asc', search} = req.query;
 
-        if (cursor) {
+    if (setOrder === 'desc' && sortBy === 'id' && !search) {
+        return res.status(500).json({
+            error: 'Não foi possível retornar a lista de jogos, pois a ordenação está decrescente e a coluna está por ID, gerando um problema na busca dos jogos.'
+        });
+    }
+
+    const orderByClause = {
+        [sortBy]: setOrder
+    };
+
+    try {
+        if (search) {
             const games = await prisma.games.findMany({
-                take: 20,
-                skip: 1,
-                cursor: {
-                    id: parseInt(cursor)
+                where: {
+                    name: {contains: search, mode: 'insensitive'}
                 }
             });
 
-            res.status(200).json({data: games, cursor: games[19].id});
+            return res.status(200).json({data: games, cursor: games[games.length - 1].id});
+        }
+
+        if (cursor) {
+            const games = await prisma.games.findMany({
+                take: take,
+                skip: 1,
+                cursor: {
+                    id: parseInt(cursor)
+                },
+                orderBy: orderByClause
+            });
+
+            return res.status(200).json({data: games, cursor: games[games.length - 1].id});
         }
 
         const games = await prisma.games.findMany({
-            take: 20
+            take: take,
+            orderBy: orderByClause
         });
 
-        res.status(200).json({data: games, cursor: games[19].id});
-
+        return res.status(200).json({data: games, cursor: games[games.length - 1].id});
     } catch (error) {
         console.error('Erro ao retornar todos os jogos do banco de dados: ', error);
-        res.status(500).json({error: 'Não foi possível retornar a lista de jogos.'});
+        return res.status(500).json({error: 'Não foi possível retornar a lista de jogos.'});
     }
 }
 
 module.exports = {
-    getAllGames,
+    getGames,
 };
