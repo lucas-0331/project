015a678800d868c17cebbeb98cf3bcdd6735d04f
Lucas Costa <lucas6.silva@alunos.ifsuldeminas.edu.br>
2025-12-15 11:47:29 -0300

    refactor: verify token has been removed from the route

--- src/routes/avatarsRoutes.js
+++ src/routes/avatarsRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const avatarController = require('../controllers/avatarController');
 const {verifyToken} = require('@/middlewares/authMiddleware');
 
-router.get('/', verifyToken, avatarController.getAvatars);
+router.get('/', avatarController.getAvatars);
 // router.post('/', verifyToken, avatarController.saveAvatar);
 // router.put('/:id', verifyToken, avatarController.updateAvatar);
 // router.delete('/:id', verifyToken, avatarController.deleteAvatar);
